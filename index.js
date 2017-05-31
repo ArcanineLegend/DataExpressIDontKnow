0a68039d9d888a5fc1c229cf90bfbd6014bcc859
Patrick Coleman <pcoleman@student.neumont.edu>
2017-05-31 10:05:16 -0600

    add pages and directory

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     pug = require('pug'),
-    path = require('path');
+    path = require('path'),
+    config = require('./tsconfig.json');
 
 var app = express();
 
@@ -8,3 +9,22 @@ app.set('view engine', 'pug');
 app.set('views', __dirname+'/views');
 app.use(express.static(path.join(__dirname+'/public')));
 
+app.get('/', function (req, res) {
+   var obj = {
+      title: "DataExpress IDontKnow"
+   };
+   res.render('index',{
+      "obj"   : obj,
+      "config": config
+   });
+});
+app.get('/:viewname', function (req, res) {
+   var obj = {
+      title: "DataExpress IDontKnow"
+   };
+   res.render(req.params.viewname, {
+      "obj" : obj,
+      "config": config
+   });
+});
+app.listen(3000);
